c778d301969a07a50a0a9de6598faea124991bd1
Leonardo Giorgiani <leo_henry@outlook.com>
2019-10-10 09:25:39 -0300

    add ci

--- utils/statistics.py
+++ utils/statistics.py
@@ -3,8 +3,8 @@ import scipy.stats
 def ci(X, confidence=0.95):
     n = len(X)
     se = desvp(X) / (n*(1/2))    
-    h = se * scipy.stats.t.ppf((1 + confidence) / 2., n-1)
-    
+    h = se * scipy.stats.t.ppf((1 + confidence) / 2, n-1)
+
     return (media(X)-h, media(X)+h)
 
 def media(X):
