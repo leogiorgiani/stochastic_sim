05a6429b6703203795b6123b9aaebe6228336469
Leonardo Giorgiani <leo_henry@outlook.com>
2019-10-31 10:13:14 -0300

    update queue models test

--- QueueModel.py
+++ QueueModel.py
@@ -3,7 +3,7 @@ from utils.Random import Congrencial as crand
 from BasicModels.Server import Server
 from BasicModels.Atoms import Client
 
-def simulate(n_caixas = 1, n_atendentes = 1, TMAX = 60, tec_g= Exponencial(4), tscaixa_g = Erlang(2,3), tsatend_g = Erlang(1.5, 4)):
+def simulate(n_caixas = 10, n_atendentes = 3, TMAX = 60, tec_g= Exponencial(4), tscaixa_g = Erlang(2,3), tsatend_g = Erlang(1.5, 4)):
     caixas = Server(n_caixas)
     atendentes = Server(n_atendentes)
 
@@ -16,10 +16,12 @@ def simulate(n_caixas = 1, n_atendentes = 1, TMAX = 60, tec_g= Exponencial(4), t
         caixas.client_arrival(cl_c)
         ts_atend = tsatend_g.x()
         atendentes.update(cl_c.spent_time)
-        t_fila_atendente = max(atendentes.attendants[caixas.getMinimalServiceTime()].getAwaitTime() - atendentes.real_time, 0)
+        t_fila_atendente = max(atendentes.attendants[atendentes.getMinimalServiceTime()].getAwaitTime() - atendentes.real_time, 0)
         cl_a = Client(cl_c.spent_time, ts_atend, atendentes.real_time, t_fila_atendente)
         atendentes.client_arrival(cl_a)
+    
+    return caixas.stats.resume(TMAX), atendentes.stats.resume(TMAX)
 
 
 if __name__ == "__main__":
-    simulate()
+    print(simulate())
--- TestQueueModel.py
+++ TestQueueModel.py
@@ -3,7 +3,7 @@ from utils.Random import Congrencial as crand
 from BasicModels.Server import Server
 from BasicModels.Atoms import Client
 
-def simulate(n_attendants = 2, TMAX = 60, tec_g = Exponencial(4), ts_g = Erlang(2,3)):
+def simulate(n_attendants = 1, TMAX = 60, tec_g = Exponencial(4), ts_g = Erlang(2,3)):
     '''
         Simula o modelo de fila de n caixas
         Retorna:    Tempo médio de serviço,
@@ -14,29 +14,15 @@ def simulate(n_attendants = 2, TMAX = 60, tec_g = Exponencial(4), ts_g = Erlang(
     '''
     sv = Server(n_attendants)
 
-    Tts = 0
-    Tspenttime = 0
-    Tqueuetime = 0
-    nClientes = 0
-    Tt_FuncL = 0
-    Tcfila = 0
     while sv.real_time < TMAX:
-        nClientes += 1
         tec = tec_g.x()
         ts = ts_g.x()
-        Tts+=ts
         sv.update(tec)
         t_fila = max(sv.attendants[sv.getMinimalServiceTime()].getAwaitTime() - sv.real_time, 0)
-        Tcfila += 1 if t_fila > 0 else 0
-        Tqueuetime += t_fila
         cl = Client(tec, ts, sv.real_time, t_fila)
-        Tspenttime += cl.spent_time
-        n, t_FuncL = sv.getFreeTime()
-        Tt_FuncL+=t_FuncL
-
         sv.client_arrival(cl)
 
-    return Tts/nClientes, Tspenttime/nClientes, Tqueuetime/nClientes, Tt_FuncL/TMAX, Tcfila/nClientes
+    return sv.stats.resume(TMAX)
 
 if __name__ == "__main__":
     crand.seed(1000)
