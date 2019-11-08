2e2983e992ed400a0c78e362dc2f289105351a09
Leonardo Giorgiani <leo_henry@outlook.com>
2019-11-08 09:29:54 -0300

    Changes in the simulations loop

--- main.py
+++ main.py
@@ -4,43 +4,42 @@ from utils.statistics import ci
 from QueueModel import simulate
 from utils.Random import Congrencial as crand
 
-'''
-    TODO: Full stats from n simulations
-'''
-
 if __name__ == "__main__":
-    for c in [1, 2, 3]:
-        for a in [1, 2, 3]:
-            sAvgSt1 = []; sAvgSt2 = []
-            sAvgSpt1 = []; sAvgSpt2 = []
-            sAvgQt1 = []; sAvgQt2 = []
-            sPFA1 = []; sPFA2 = []
-            sPAQ1 = []; sPAQ2 = []
-        
-            for i in Bar('Simulando para {} caixas e {} atendentes de lanche'.format(c, a)).iter(range(5000)):
-                crand.seed()
-                
-                (avgSt1, avgSpt1, avgQt1, pFA1, pAQ1),\
-                (avgSt2, avgSpt2, avgQt2, pFA2, pAQ2) = simulate(c,a,30)
-                
-                sAvgSt1.append(avgSt1); sAvgSpt1.append(avgSpt1); sAvgQt1.append(avgQt1); sPFA1.append(pFA1); sPAQ1.append(pAQ1)
-                sAvgSt2.append(avgSt2); sAvgSpt2.append(avgSpt2); sAvgQt2.append(avgQt2); sPFA2.append(pFA2); sPAQ2.append(pAQ2)
+    n_atendentes = [(3,6), (4,5), (5,4), (6,3)]
 
-            print("Resultados para {} caixas e {} atendentes de lanche".format(c,a))
+    for c,a in n_atendentes:
+        sAvgSt1 = []; sAvgSt2 = []
+        sAvgSpt1 = []; sAvgSpt2 = []
+        sAvgQt1 = []; sAvgQt2 = []
+        sPFA1 = []; sPFA2 = []
+        sPAQ1 = []; sPAQ2 = []
+    
+        for i in Bar('Simulando para {} caixas e {} atendentes de lanche'.format(c, a)).iter(range(5000)):
+            crand.seed()
+            
+            (avgSt1, avgSpt1, avgQt1, pFA1, pAQ1),\
+            (avgSt2, avgSpt2, avgQt2, pFA2, pAQ2) = simulate(c,a,30)
+            
+            sAvgSt1.append(avgSt1); sAvgSpt1.append(avgSpt1); sAvgQt1.append(avgQt1); sPFA1.append(pFA1); sPAQ1.append(pAQ1)
+            sAvgSt2.append(avgSt2); sAvgSpt2.append(avgSpt2); sAvgQt2.append(avgQt2); sPFA2.append(pFA2); sPAQ2.append(pAQ2)
 
-            print("""Fila 1: \n
-                \tTempo médio de serviço: {}\n
-                \tTempo médio gasto no sistema: {}\n
-                \tTempo médio de fila: {}\n
-                \tProbabilidade de atendente livre: {}\n
-                \tProbabilidade de esperar na fila  {}\n
-                """.format(ci(sAvgSt1), ci(sAvgSpt1), ci(sAvgQt1), ci(sPFA1), ci(sPAQ1)))
+        print("Resultados para {} caixas e {} atendentes de lanche".format(c,a))
+
+        print("""Fila 1: \n
+            \tTempo médio de serviço: {}\n
+            \tTempo médio gasto no sistema: {}\n
+            \tTempo médio de fila: {}\n
+            \tProbabilidade de atendente livre: {}\n
+            \tProbabilidade de esperar na fila  {}\n
+            """.format(ci(sAvgSt1), ci(sAvgSpt1), ci(sAvgQt1), ci(sPFA1), ci(sPAQ1)))
+    
+        print("""Fila 2: \n
+            \tTempo médio de serviço: {}\n
+            \tTempo médio gasto no sistema: {}\n
+            \tTempo médio de fila: {}\n
+            \tProbabilidade de atendente livre: {}\n
+            \tProbabilidade de esperar na fila  {}\n
+            """.format(ci(sAvgSt2), ci(sAvgSpt2), ci(sAvgQt2), ci(sPFA2), ci(sPAQ2)))
+
         
-            print("""Fila 2: \n
-                \tTempo médio de serviço: {}\n
-                \tTempo médio gasto no sistema: {}\n
-                \tTempo médio de fila: {}\n
-                \tProbabilidade de atendente livre: {}\n
-                \tProbabilidade de esperar na fila  {}\n
-                """.format(ci(sAvgSt2), ci(sAvgSpt2), ci(sAvgQt2), ci(sPFA2), ci(sPAQ2)))
     
