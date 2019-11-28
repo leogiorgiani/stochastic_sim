0890d458be2b5ef80e36ea99e1673af1867d90b6
Leonardo Giorgiani <leo_henry@outlook.com>
2019-11-28 18:38:47 -0300

    add print for csv analysis

--- TestQueueModel.py
+++ TestQueueModel.py
@@ -3,13 +3,14 @@ from utils.Random import Congrencial as crand
 from BasicModels.System import System
 from BasicModels.Atoms import Client
 
-def simulate(n_attendants = 1, TMAX = 30, tec_g = Exponencial(4), ts_g = Erlang(2,3)):
+def simulate(n_attendants = 2, TMAX = 30, tec_g = Exponencial(4), ts_g = Erlang(2,3)):
     '''
             Simulates the queue model of 1 System with n attendents,
         Returns the stats from simulation.
         
         ** See System Documentation
     '''
+    print("tec,ts,treal,tinit,tfim,tfila,tsist,tCLivre,n_Fila")
     sv = System(n_attendants)
 
     while sv.real_time < TMAX:
@@ -18,6 +19,11 @@ def simulate(n_attendants = 1, TMAX = 30, tec_g = Exponencial(4), ts_g = Erlang(
         sv.update(tec)
         cl = Client(tec, ts, sv.real_time, sv.getMinimalAwaitTime())
         sv.client_arrival(cl)
+        print("{:.2f}, {:.2f}, {:.2f}, {:.2f}, {:.2f}, {:.2f}, {:.2f}, {:.2f}, {}".format(
+            tec, ts, sv.real_time, cl.start_time, cl.end_time, 
+            cl.queue_time, cl.spent_time, sv.getFreeTime(), sv.getQueuesSize()
+        ))
+
 
     return sv.stats.resume(TMAX)
 
